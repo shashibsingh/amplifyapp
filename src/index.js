ece48b527386c39b7e756aa8647d78cf251e5a4e
Shashi Singh <sbsingh@us.ibm.com>
2020-09-28 12:38:30 -0700

    Fixed import in index.js changed upper case

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app.js';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
